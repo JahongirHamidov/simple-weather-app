8c99b26914dc6e4e22799edff21a4745b2343aaf
JahongirHamidov <hamidovjahongir444@gmail.com>
2020-12-26 19:12:26 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,78 +1,124 @@
-import React,{useState,useContext} from 'react'
-import './App.css'
-import {Weather} from './components/Weather'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'weather-icons/css/weather-icons.css'
-import {Form} from './components/Form'
-import {Provider} from './globalState'
-import {GlobalState} from './globalState'
+import React from "react"
+import "./App.css"
+import Form from "./app_component/form.component"
+import Weather from "./app_component/weather.component"
+import "bootstrap/dist/css/bootstrap.min.css"
 
-const api = {
-  key: '9d338d844a7bffd71902ffe0d3b6a201',
-  base: 'https://api.openweathermap.org/data/2.5/'
+// git project https://github.com/erikflowers/weather-icons
+import "weather-icons/css/weather-icons.css"
+
+const Api_Key = "9d338d844a7bffd71902ffe0d3b6a201"
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      city: undefined,
+      country: undefined,
+      icon: undefined,
+      main: undefined,
+      celsius: undefined,
+      temp_max: null,
+      temp_min: null,
+      description: "",
+      error: false
+    }
+
+    this.weatherIcon = {
+      Thunderstorm: "wi-thunderstorm",
+      Drizzle: "wi-sleet",
+      Rain: "wi-storm-showers",
+      Snow: "wi-snow",
+      Atmosphere: "wi-fog",
+      Clear: "wi-day-sunny",
+      Clouds: "wi-day-fog"
+    }
+  }
+
+  get_WeatherIcon(icons, rangeId) {
+    switch (true) {
+      case rangeId >= 200 && rangeId < 232:
+        this.setState({ icon: icons.Thunderstorm })
+        break
+      case rangeId >= 300 && rangeId <= 321:
+        this.setState({ icon: icons.Drizzle })
+        break
+      case rangeId >= 500 && rangeId <= 521:
+        this.setState({ icon: icons.Rain })
+        break
+      case rangeId >= 600 && rangeId <= 622:
+        this.setState({ icon: icons.Snow })
+        break
+      case rangeId >= 701 && rangeId <= 781:
+        this.setState({ icon: icons.Atmosphere })
+        break
+      case rangeId === 800:
+        this.setState({ icon: icons.Clear })
+        break
+      case rangeId >= 801 && rangeId <= 804:
+        this.setState({ icon: icons.Clouds })
+        break
+      default:
+        this.setState({ icon: icons.Clouds })
+    }
+  }
+
+  calCelsius(temp) {
+    let cell = Math.floor(temp - 273.15)
+    return cell
+  }
+
+  getWeather = async e => {
+    e.preventDefault()
+
+    const country = e.target.elements.country.value
+    const city = e.target.elements.city.value
+
+    if (country && city) {
+      const api_call = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`
+      )
+
+      const response = await api_call.json()
+
+      this.setState({
+        city: `${response.name}, ${response.sys.country}`,
+        country: response.sys.country,
+        main: response.weather[0].main,
+        celsius: this.calCelsius(response.main.temp),
+        temp_max: this.calCelsius(response.main.temp_max),
+        temp_min: this.calCelsius(response.main.temp_min),
+        description: response.weather[0].description,
+        error: false,
+      })
+
+      // seting icons
+      this.get_WeatherIcon(this.weatherIcon, response.weather[0].id)
+
+      console.log(response)
+    } else {
+      this.setState({
+        error: true
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Form loadweather={this.getWeather} error={this.state.error} />
+        <Weather
+          cityname={this.state.city}
+          weatherIcon={this.state.icon}
+          temp_celsius={this.state.celsius}
+          temp_max={this.state.temp_max}
+          temp_min={this.state.temp_min}
+          description={this.state.description}
+        />
+      </div>
+    )
+  }
 }
 
-export default function App() {
-
-  const [state,setState] = useState(null)
-  const [weatherIcon, setWeatherIcon] = React.useState(null)
-  //const {city, country} = useContext(GlobalState)
-
-  
-    
-    function getWeatherIcon(icons,rangeId){
-        switch(true){
-            case rangeId>=200 && rangeId<=232:
-                setWeatherIcon(icons.Thunderstorm)
-            break
-            case rangeId>=300 && rangeId<=321:
-                setWeatherIcon(icons.Drizzle)
-            break
-            case rangeId>=500 && rangeId<=531:
-                setWeatherIcon(icons.Rain)
-            break
-            case rangeId>=600 && rangeId<=622:
-                setWeatherIcon(icons.Snow)
-            break
-            case rangeId>=700 && rangeId<=781:
-                setWeatherIcon(icons.Atmosphere)
-            break
-            case rangeId==800:
-                setWeatherIcon(icons.Clear)
-            break
-            case rangeId>=801 && rangeId<=804:
-                setWeatherIcon(icons.Cloud)
-            break
-            default: 
-              setWeatherIcon(icons.Cloud)
-        }
-    }
-    
-    const icons = {
-        Thunderstorm: 'wi-thunderstorm',
-        Drizzle: 'wi-sleet',
-        Rain: 'wi-storm-showers',
-        Snow: 'wi-snow',
-        Atmosphere: 'wi-fog',
-        Clear: 'wi-day-sunny',
-        Cloud: 'wi-day-fog'
-    }
-    //getWeatherIcon(icons, data.weather[0].id )
-
-  const getWeather = async () =>{
-      await fetch(`${api.base}weather?q=berlin,germany&appid=${api.key}`)
-        .then(data=> data.json() )
-        .then(json=>{ setState(json) })
-        .catch(err=> console.log(err) )
-  }
-
-console.log(state)
-  return (
-    <Provider>
-      <Form loadWeather={getWeather} />
-      { state ? <Weather data={state} icon={icons} /> : <p>wait</p>}
-
-    </Provider>
-  )
-  }
+export default App
 
